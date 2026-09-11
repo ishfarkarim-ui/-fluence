aa21fa65e519102b8f07482fb0f60a4b5e01ca2a
ishfarkarim-ui <ishfarkarim@gmail.com>
2026-09-12 03:24:00 +0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Status", // required by Discord API, not shown in the client
         state: "stalking nirv",     // this is what people actually see
         type: 2,               // Custom
       },
